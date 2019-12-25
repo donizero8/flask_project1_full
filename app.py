18ac1d491056184bc71b1f665d082822910d2836
donizero8 <doni.zero8@gmail.com>
2019-12-25 22:03:21 +0700

    update full project pert 2

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, abort, request, render_template, redirect, url_for, \
+    session, flash
+from flask_bootstrap import Bootstrap
+from forms import NameForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hardToGuessString'
+bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello, Flask!'
-
-@app.route('/hello/<name>')
-def hello(name):
-	return 'Hello, {}'.format(name)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+    
+@app.route('/hello/')
+def hello(name=None):
+    if name is None:
+        name=request.args.get("name")
+        if name:
+            return render_template('user.html', name=name)
+        else:
+    	    abort(404)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
